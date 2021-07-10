1ae28226291b66272df8a3a2045e8a35597d26b7
Dmitry <dmitry.balduev@gmail.com>
2021-07-10 00:18:14 -0400

    added initial form

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -3,8 +3,30 @@ import React from 'react';
 
 function Contact(){
 
+    function renderSubmit(event) {
+        event.preventDefault();
+      };
+
     return (
-        <div>Contact</div>
+        <div>
+            <p>Contact form:</p>
+
+            <form>
+                <label>Name: </label>
+                <br></br>
+                <input id='nameInput' placeholder='Enter your name'></input>
+                <br></br>
+                <label>Email: </label>
+                <br></br>
+                <input id='emailInput' placeholder='Enter your email'></input>
+                <br></br>
+                <label>Message: </label>
+                <br></br>
+                <input id='messageInput' placeholder='Enter your message'></input>
+                <br></br>
+                <button id='contactSubmitButton' type="button"  onClick={renderSubmit} >Submit</button>
+            </form>
+        </div>
     )
 }
 export default Contact;
