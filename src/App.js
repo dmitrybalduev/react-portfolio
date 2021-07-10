285a949a5ad2d6d1b7fe10ee7a3967488641da0f
Dmitry <dmitry.balduev@gmail.com>
2021-07-10 00:04:56 -0400

    added routes in app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About'
+import Contact from './components/Contactform';
+import Portfolio from './components/Portfolio';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
-  return <About/>
+  return (
+    <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Route exact path="/">
+              <About />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            <Route exact path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route exact path="/resume">
+              <Resume />
+            </Route>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+  )
 }
 
 export default App;
