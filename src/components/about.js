73c0a2ac65139cd23674f7a51517b2e9ecaa74a3
Dmitry <dmitry.balduev@gmail.com>
2021-07-10 00:04:03 -0400

    added initial components

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+
+function About() {
+    const message = 'Here is some text about me';
+
+    return (
+        <div>
+            <section>
+                <h2>Dmitry</h2>
+                <p>Here is some text about me</p>
+            </section> 
+        </div>
+    );
+  }
+  
+  export default About;
