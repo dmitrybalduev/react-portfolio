e388a3636af7b713d0f0e7681d69541c5269cdc4
Dmitry <dmitry.balduev@gmail.com>
2021-07-12 23:18:41 -0400

    added link to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import hooks from React Router
 import { useLocation, useHistory } from 'react-router-dom';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   // We retrieve the current `location` object data from React Router
@@ -20,6 +21,12 @@ const Footer = () => {
           </button>
         )}
         <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
+        <a href="https://github.com/dmitrybalduev" target="_blank">
+          <FaGithub size="2em" color="#343a40"/>
+        </a>
+        <a href="https://linkedin.com/" target="_blank">
+          <FaLinkedin size="2em" color="#343a40"/>
+        </a>
       </div>
     </footer>
   );
