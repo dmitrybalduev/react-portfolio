f74d55471df2b59cb33de023255c115af6219a22
Dmitry <dmitry.balduev@gmail.com>
2021-07-14 22:20:44 -0400

    add gh pages, and added resume link

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,9 +5,9 @@ function Resume(){
 
     return (
         <div >
-            <a href="https://github.com/dmitrybalduev">
+            <a href="https://docs.google.com/document/d/1KUQLO0qb8JhNyt4yA59sWy4Q7hZTE7BKRU8D0nNJDK8/edit?usp=sharing" target='_blank'>
                 <button type="button" class="btn btn-warning btn-lg btn-block"
-                style={{height:'50%', marginTop:'15%', marginBottom:'10%' }}>Click here to download my resume
+                style={{height:'50%', marginTop:'15%', marginBottom:'10%' }}>Click here to view my resume
                 </button>
             </a>
         </div>
