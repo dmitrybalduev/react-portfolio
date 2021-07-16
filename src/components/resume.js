4f02d8a6b41281d6321e0a945e818fabe4368d61
Dmitry <dmitry.balduev@gmail.com>
2021-07-15 22:43:20 -0400

    added downloadable link for resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,7 +5,12 @@ function Resume(){
 
     return (
         <div >
-            <a href="https://docs.google.com/document/d/1KUQLO0qb8JhNyt4yA59sWy4Q7hZTE7BKRU8D0nNJDK8/edit?usp=sharing" target='_blank'>
+            {/* <a href="https://docs.google.com/document/d/1KUQLO0qb8JhNyt4yA59sWy4Q7hZTE7BKRU8D0nNJDK8/edit?usp=sharing" target='_blank'>
+                <button type="button" class="btn btn-warning btn-lg btn-block"
+                style={{height:'50%', marginTop:'15%', marginBottom:'10%' }}>Click here to view my resume
+                </button>
+            </a> */}
+            <a href="./Assets/Dmitry Balduev, Resume.pdf" download>
                 <button type="button" class="btn btn-warning btn-lg btn-block"
                 style={{height:'50%', marginTop:'15%', marginBottom:'10%' }}>Click here to view my resume
                 </button>
