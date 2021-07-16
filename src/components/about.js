42c3881305fb6463838d412cec3f4a360122c60e
Dmitry <dmitry.balduev@gmail.com>
2021-07-16 00:00:47 -0400

    modified fonts, edit navbar

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Header from './Header';
+import Header from './Header';
 import Image from 'react-bootstrap/Image'
 import pic from './Assets/pic3.png'
 
@@ -8,7 +8,7 @@ function About() {
 
     return (
         <div>
-            <section style={{justifyContent:'center'}}>
+            <section style={{justifyContent:'center', fontFamily: "Uchen, serif", fontStyle:"bold"}}>
                 <h2>Dmitry</h2>
                 <Image src={pic} roundedCircle style={{margin:'1em'}}/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Header(){
     return (
-        <Navbar  expand="lg" style={{ marginBottom: '3rem' , background:'#e9c46a'} }>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Link to="/">About Me</Link>
-                    <Link to="portfolio">Portfolio</Link>
-                    <Nav.Link href="resume">Resume</Nav.Link>
-                    <Nav.Link href="contact">Contact Me</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        
+        // <Navbar  expand="lg" style={{ marginBottom: '3rem' , background:'#e9c46a'} }>
+        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //     <Navbar.Collapse id="basic-navbar-nav">
+        //         <Nav className="mr-auto">
+        //             <Link to="/">About Me</Link>
+        //             <Link to="portfolio">Portfolio</Link>
+        //             <Link to="resume">Resume</Link>
+        //             <Link to="contact">Contact Me</Link>
+        //         </Nav>
+        //     </Navbar.Collapse>
+        // </Navbar>
+/////////////////////////
+    
+/////////////////////////
+  
+    // <ul class="nav nav-tabs" id="myTab" role="tablist">
+    //     <li class="nav-item">
+    //         <NavLink to="resume">
+    //         <a class="nav-link active" id="home-tab" data-toggle="tab" to="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+    //         </NavLink>
+    //     </li>
+    //     <li class="nav-item">
+    //     <NavLink to="contact">
+    //         <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+    //         </NavLink>
+    //     </li>
+    //     <li class="nav-item">
+    //         <a class="nav-link" id="contact-tab" data-toggle="tab" href="resume" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+    //     </li>
+       
+    // </ul>
+
+    <nav className="nav nav-pills flex-column flex-sm-row" expand="lg" style={{ marginBottom: '3rem' , background:'#e9c46a', fontFamily: "Zen Tokyo Zoo, cursive"} }>
+        <NavLink to="/">
+            <a className="flex-sm-fill text-sm-center nav-link " >About Me</a>
+        </NavLink>
+        <NavLink to="portfolio">
+            <a className="flex-sm-fill text-sm-center nav-link" >Portfolio</a>
+        </NavLink>
+        <NavLink to="resume">
+            <a className="flex-sm-fill text-sm-center nav-link" >Resume</a>
+        </NavLink>
+        <NavLink to="contact">
+            <a className="flex-sm-fill text-sm-center nav-link">Contact</a>
+        </NavLink>
+    </nav>
+
+
     )
 }
 export default Header;
